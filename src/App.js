91007d6247abdec2216757550bc683bbfddd1aca
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-04-25 19:24:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,14 @@ import './App.css';
 function App() {
   return (
     <div>
-     
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/tours" element={<About />} />
+        </Routes>
+        <Footer />
+      </Router>
     </div>
   );
 }
