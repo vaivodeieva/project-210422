9ac871bc8d4d0c0fe7f0e012f3cbab456de9a735
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-05-10 16:13:49 +0300

    Update Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -12,6 +12,19 @@ function Header() {
                 <div className="mainmenu">
                     <nav className="main-nav">
                         <ul>
+                            <a href="/">
+                                <li>
+                                    <svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+                                        <path fill="none" stroke="red"
+                                            d="M 10,30
+       A 20,20 0,0,1 50,30
+       A 20,20 0,0,1 90,30
+       Q 90,60 50,90
+       Q 10,60 10,30 z" />
+                                    </svg>
+                                    <p>Home</p>
+                                </li>
+                            </a>
                             <a href="/discover">
                                 <li>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">
