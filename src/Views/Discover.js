7a1d8b523ed7e24e502bbb7b5c3b3fa6707b6b58
vaivodeieva <78384667+vaivodeieva@users.noreply.github.com>
2022-05-24 22:00:18 +0300

    Update Discover.js

--- src/Views/Discover.js
+++ src/Views/Discover.js
@@ -2,6 +2,237 @@ function Discover() {
 
     return (
         <div>
+            <div className="container  wow fadeInUp" id="imagesContainer">
+                {/* <!-- row -start --> */}
+                <div className="row" id="row1">
+                    <div className="col-sm-4 col-md-2 img-col">
+
+                        <div className="fill wow fadeInUp" data-wow-delay="100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4  col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1500ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="500ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1700ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="700ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1900ms" data-wow-duration="1s">
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="900ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="2100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="1100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="2300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                </div>
+                {/* <!-- row -end --> */}
+
+                {/* <!-- row -start --> */}
+                <div className="row" id="row1">
+                    <div className="col-sm-4 col-md-2 img-col">
+
+                        <div className="fill wow fadeInUp" data-wow-delay="100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4  col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1500ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="500ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1700ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="700ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1900ms" data-wow-duration="1s">
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="900ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="2100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="1100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="2300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                </div>
+
+                <div className="row" id="row1">
+                    <div className="col-sm-4 col-md-2 img-col">
+
+                        <div className="fill wow fadeInUp" data-wow-delay="100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4  col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1500ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="500ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1700ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="700ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="1900ms" data-wow-duration="1s">
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="900ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Custom Exterior" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="2100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/253x316" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                    <div className="col-sm-4 col-md-2 img-col">
+                        <div className="fill wow fadeInUp" data-wow-delay="1100ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x298" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                        <div className="fill wow fadeInUp" data-wow-delay="2300ms" data-wow-duration="1s">
+
+                            <img src="http://via.placeholder.com/239x179" alt="Exterior Paintjob" className="img-thumbnail-gallery" />
+
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
 
         </div>
     )
